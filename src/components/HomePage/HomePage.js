09472e86dec4bf727062a0e7b93b897209bd4fc6
happyminion <alexa.novikova@gmail.com>
2018-11-06 22:25:42 +0200

    styling hero and input areas

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -10,7 +10,6 @@ class HomePage extends Component {
             <h1>What's for dinner today?</h1>
             <form>
               <label>
-                Name:
                 <input className="home-order-input" type="text" name="name" value="Enter your street address here" />
               </label>
             </form>
